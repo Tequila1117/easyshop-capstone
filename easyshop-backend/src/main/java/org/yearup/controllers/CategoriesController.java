1c1c13bb38aaf3f46b80781b1486a625f882b8d4
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-12-18 15:16:24 -0500

    added annontations and call methods for admin use of deleting/update a category

--- easyshop-backend/src/main/java/org/yearup/controllers/CategoriesController.java
+++ easyshop-backend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -77,9 +77,11 @@ public class CategoriesController
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
-        // update the category by id
+    @PutMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        categoryDao.update(id, category);   // update the category by id
+
     }
 
 
